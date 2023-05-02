1ef86e0354b86a5cd3688fe6f899f50b25302819
ManuelF <manuel.floriani1999@gmail.com>
2023-05-02 14:49:44 +0200

    Creazione html, css e js

---
+++ main.js
@@ -0,0 +1,50 @@
+// seleziono elemento contenitore
+const contenitore = document.getElementById("contenitore");
+console.log(contenitore);
+
+// ciclo for per creare elementi e output
+for(let i = 1; i <= 100; i++){
+    
+    
+    // creare nuovo elemento
+    const quadrato = document.createElement('span');
+    
+    
+    // aggiungere classe ad ogni span
+    quadrato.classList.add('square')
+    
+    
+    // inserire il valore all'interno dell'elemento creato
+    // quadrato.append(i);
+    
+    // se l'elemento è divisibile per tre
+    if(i % 3 == 0){
+        quadrato.append = `fizz`
+    }
+    // se l'elemento è divisibile per 5
+    else if(i % 5 == 0){
+        quadrato.append =`buzz`
+    }
+    // se l'elemento è divisibile sia per 3 che per 5
+    else if(i % 15 == 0){
+        quadrato.append = "fizzbuzz"
+    }
+    // altrimenti scrivi il numero
+    else{
+        quadrato.append(i);
+    }
+
+
+    // aggiungere al contenitore l'elemento creato
+    contenitore.append(quadrato);
+
+
+}
+
+
+// // for (var i = 1; i < 101; i++) {
+//     if (i % 15 == 0) console.log("FizzBuzz");
+//     else if (i % 3 == 0) console.log("Fizz");
+//     else if (i % 5 == 0) console.log("Buzz");
+//     else console.log(i);
+// }
